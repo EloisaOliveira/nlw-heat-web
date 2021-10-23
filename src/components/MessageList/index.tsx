0d75ccb7221ff624db2b76f84becbf5b07ae1044
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2021-10-23 16:17:07 -0300

    feat: replacing in-memory data with API data

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,42 +1,46 @@
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+
 import styles from './styles.module.scss'
 
 import logoImg from '../../assets/logo.svg'
 
-export function MessageList(){
+type Message = {
+  id: string
+  text: string
+  user: {
+    name: string
+    avatar_url: string
+  }
+}
+
+export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data)
+    })
+  }, [])
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/EloisaOliveira.png" alt="Eloisa de Oliveira" />
-            </div>
-            <span>Eloisa de Oliveira</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/EloisaOliveira.png" alt="Eloisa de Oliveira" />
-            </div>
-            <span>Eloisa de Oliveira</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/EloisaOliveira.png" alt="Eloisa de Oliveira" />
-            </div>
-            <span>Eloisa de Oliveira</span>
-          </div>
-        </li>
+        {messages.map(message => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src="https://github.com/EloisaOliveira.png" alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   )
